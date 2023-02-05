ab3b734f4a85f502c2cc289deca24b95c99930f2
roshan <roshanprusty100@gmail.com>
2023-02-05 15:56:46 +0530

    Project-setup & some other files added

---
+++ React 2/project-setup/src/Components/Main.js
@@ -0,0 +1,16 @@
+import MainStyle from "./MainStyle.css";
+function Main(){
+    return<>
+    <main>
+    <h2 className="main--title">Fun Facts about React</h2>
+    <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originaly created by Jordan Walke</li>
+        <li>Has well over 100k stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+    </main>
+    </>
+}
+export default Main;
---
+++ React 2/project-setup/src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import NavbarStyle from "./NavbarStyle.css"
+function Navbar(){
+    return (
+        <>
+        <nav>
+            <img src="" alt="Image not yet attached" className="nav--logo_text" />
+            <h3 className="Nav--logo_text">ReactFActs</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
+        </>
+    )
+}
+export default Navbar;
