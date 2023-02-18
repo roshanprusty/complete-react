6e1a93c7fb0de042abb9cdc8702862432d8a46a2
roshan <roshanprusty100@gmail.com>
2023-02-18 22:38:10 +0530

    3-react-effect-reducers some files added

---
+++ React1/3-react-effect-reducers/use-effect-demo-app/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Navbar from './components/Navbar';
+import Form from './components/Form';
+
+function App() {
+  return(<>
+  <Navbar />
+  <Form />
+  </>)
+  
+}
+
+export default App
---
+++ React1/3-react-effect-reducers/use-effect-demo-app/src/components/Form.jsx
@@ -0,0 +1,13 @@
+import './Form.css';
+function Form(){
+    return(<>
+    {/* <form>
+        <label for="name">Name</label>
+        <input type="text"></input>
+        <label for="password">password</label>
+        <input type="password"></input>
+    </form> */}
+    <h1>Hello</h1>
+    </>);
+}
+export default Form;
