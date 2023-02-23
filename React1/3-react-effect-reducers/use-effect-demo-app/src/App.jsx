bdd9a622f1770f790def1a4ad7affe37e63e242c
roshan <roshanprusty100@gmail.com>
2023-02-23 23:53:15 +0530

    more files added to react effect folder

--- React1/3-react-effect-reducers/use-effect-demo-app/src/App.jsx
+++ React1/3-react-effect-reducers/use-effect-demo-app/src/App.jsx
@@ -1,12 +1,25 @@
 import './App.css'
-import Navbar from './components/Navbar';
-import Form from './components/Form';
-
+import { useState } from 'react';
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
 function App() {
-  return(<>
-  <Navbar />
-  <Form />
-  </>)
+  const [isLoggedIn,updateIsLoggedIn] = useState(false);
+  const loginHandler = (email,password) =>{
+    updateIsLoggedIn(true);
+  }
+  const logoutHandler=() =>{
+    updateIsLoggedIn(false);
+  }
+  return (
+    <>
+      <MainHeader  />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </>
+);
   
 }
 
