241a44f74e4cf7de4452055c5305cc6118186e6f
roshan <roshanprusty100@gmail.com>
2023-03-01 23:11:04 +0530

    files added to Hooks folder

---
+++ React1/Hooks/1-use-state/src/components/Counter.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import './Counter.css'
+
+function Counter(){
+    let[count, setcount] = useState(0);
+    let countValue=true;
+    function inc(){
+        setcount(count+1);
+    }
+    function dec(){
+        setcount(count-1);
+    }
+    if(count!==0) countValue=false;
+    return(<div className="incdec">
+        <h2>Counter (Number)</h2>
+        <button onClick={inc}>INCREMENT</button>
+        <p>{count}</p>
+        <button onClick={dec} disabled={countValue}>DECREMENT</button>
+
+
+    </div>)
+}
+export default Counter;
