8cb762449c7c22539562fdf1c55214c88ba54c8d
roshan <roshanprusty100@gmail.com>
2023-04-07 20:21:48 +0530

    useffect folder  created

---
+++ React1/Hooks/3-useEffect/src/App.jsx
@@ -0,0 +1,8 @@
+import CompA from "./components/CompA";
+
+function App(){
+  return(<>
+    <CompA />
+  </>)
+}
+export default App;
---
+++ React1/Hooks/3-useEffect/src/components/CompA.jsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+function CompA(){
+    let[counter, setCounterValue]=useState(0);
+    let[counter2, setCounterValue2]=useState(0);
+    // function mounted(){ console.log("component mounted")}
+    // mounted();
+    
+    // useEffect(()=>{console.log("!!!")})
+    // useEffect(()=>{console.log("!!!")},[])
+    useEffect(()=>{console.log("!!!")},[counter2])
+    return(<>
+    <p>{counter}</p>
+    <p>{counter2}</p>
+    <button onClick={(()=>setCounterValue(counter+1))}>click Me1</button>
+    <button onClick={(()=>setCounterValue2(counter2+1))}>click Me2</button>
+    </>)
+}
+export default CompA;
