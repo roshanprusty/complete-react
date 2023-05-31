78eaef5af04fbabb6a9b588805800041cc3b99fa
roshan <roshanprusty100@gmail.com>
2023-05-31 23:58:05 +0530

    -

--- Demo/src/App.jsx
+++ Demo/src/App.jsx
@@ -1,12 +1,23 @@
-import Button from './Component/Button';
+import { useState,useRef } from "react";
+import axios from 'axios'
 
-const App = () => {
-  return (
-    <div>
-      <h1>My App</h1>
-      <Button />
-    </div>
-  );
-};
+function App(){
+  let fname = useRef();
+  let lname = useRef();
+
+  function AddUsers(){
+    axios.post("https://fir-897b5-default-rtdb.firebaseio.com/")
+  }
+  return(<>
+    <form>
+      <input type="text" placeholder="First Name" ref={fname}/>
+      <input type="text" placeholder="Last Name" ref={lname}/>
+      <br />
+      <button type="Submit" onClick={AddUsers}>Add</button>
+      <button type="Submit">GET</button>
+
+    </form>
+  </>)
+}
 
 export default App;
