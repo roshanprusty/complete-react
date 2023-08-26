cbffb6dc711a858b25324041203e887ca1fcf6fb
roshan <roshanprusty100@gmail.com>
2023-08-26 22:17:27 +0530

    react-router

--- React1/React-Router/react-router-v6/src/App.jsx
+++ React1/React-Router/react-router-v6/src/App.jsx
@@ -1,13 +1,19 @@
 import { ReactDOM } from "react-dom";
-import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./Components/Home";
 import './App.css';
+import Learn from "./Components/Learn";
 function App(){
   return(<>
     {/* <Home /> */}
     <Router>
       <Routes>
-        <Route path="/check" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/learn" element={<Learn />} />
+
+        {/* when you route to /myapp it will go to /learn  */}
+        <Route path="/myapp" element={<Navigate to= "/Learn" />} /> 
+        <Route path="/myapp2" element={<Navigate replace to= "/Learn" />} /> 
       </Routes>
     </Router>
   </>)
---
+++ React1/React-Router/react-router-v6/src/Components/Learn.jsx
@@ -0,0 +1,7 @@
+function Learn(){
+    return (<>
+        <h2>It's learn page</h2>
+        <p>Here all courses are listed here</p>
+    </>)
+}
+export default Learn;
